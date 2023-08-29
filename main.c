141066022224e85404032d6990b9ad2e1ba72018
Miguel Botelho <135281223+Bot-e-lho@users.noreply.github.com>
2023-08-29 17:58:55 -0300

    Update main.c

--- main.c
+++ main.c
@@ -3,10 +3,58 @@
 #include <string.h>
 #include <time.h>
 
-int b_Off;
-int b_Ind;
-int b_Tag;
+int Off;
+int Ind;
+int Tag;
 
-typedef struct cache {
-    int val;
+typedef struct Bloco {
+    unsigned int val;
+    unsigned int tag;
+} Bloco;
+
+typedef struct Conj {
+    Bloco* conjunto;
+} Conj;
+
+typedef struct Cache {
+    int nsets;
+    int bsize;
+    int assoc;
+    char subst;
+    Conj* Conjs;
 } Cache;
+
+typedef struct CacheStats {
+    int acessos;
+    int hits;
+    int misses;
+    int comp_misses;
+    int cap_misses;
+    int conf_misses;
+} CacheStats;
+
+int main(int argc, char *argv[]) {
+    if (argc != 7) {
+        printf("Padrao utilizado: <nsets> <bsize> <assoc> <substituição> <flag_saida> arquivo_de_entrada\n");
+        return 1;
+    }
+
+    int nsets = atoi(argv[1]);
+    int bsize = atoi(argv[2]);
+    int assoc = atoi(argv[3]);
+    char subst = argv[4][0];
+    int flag_saida = atoi(argv[5]);
+    char *file = argv[6];
+
+    Cache cache;
+    cache.nsets = nsets;
+    cache.bsize = bsize;
+    cache.assoc = assoc;
+    cache.subst = subst;
+
+
+    FILE *arq = fopen(file, "rb");
+    if (!arq) {
+        printf("Erro\n");
+        return 1;
+    }
